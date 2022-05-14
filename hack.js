e31634d90cf3fbf8ff71487b7228510d4e0f0469
vanflux <lucasabruzzi@hotmail.com>
2022-05-14 01:42:20 -0300

    UI added + legit solving

--- hack.js
+++ hack.js
@@ -29,6 +29,8 @@
   }) || [];
   const [,bombVarName] = findGlobal(x => x.toString().match(/\[\w+\.x\]\[\w+\.y\]\.(\w+)\=!0,/)) || [];
   const [,flagVarName] = findGlobal(x => x.toString().match(/\!\w+\.(\w+)\?"DETONATED_MINE"/)) || [];
+  const [,numVarName] = findGlobal(x => x.toString().match(/0===\w+\.\w+\[\w+\.x\]\[\w+\.y\]\.(\w+)/)) || [];
+  
 	const gameLoopFuncName = findGlobal((x, key) => {
     const str = x.toString();
     if (str.includes('this.startTime') && str.includes('this.canvas.height')) return key;
@@ -43,6 +45,7 @@
 	console.log('alreadyOpenedVarName', alreadyOpenedVarName);
 	console.log('bombVarName', bombVarName);
 	console.log('flagVarName', flagVarName);
+  console.log('numVarName', numVarName);
   console.log('gameLoopFuncName', gameLoopFuncName);
 
   if (!leftVarName || !rightVarName || !middleVarName || !inputFuncName || !mapVarName || !alreadyOpenedVarName || !bombVarName || !flagVarName || !gameLoopFuncName)
@@ -77,6 +80,7 @@
     const map = extractMap(instance);
     const {x: clickX, y: clickY} = posToClickPos(map, x, y);
     window[inputFuncName](instance, clickX, clickY);
+    instance[leftVarName] = false;
   }
   
   function rightClick(instance, x, y) {
@@ -86,6 +90,7 @@
     const map = extractMap(instance);
     const {x: clickX, y: clickY} = posToClickPos(map, x, y);
     window[inputFuncName](instance, clickX, clickY);
+    instance[rightVarName] = false;
   }
   
   function extractMap(instance) {
@@ -97,6 +102,7 @@
       alreadyOpened: raw[alreadyOpenedVarName],
       bomb: raw[bombVarName],
       flag: raw[flagVarName],
+      num: raw[numVarName],
     };
 	}
   
@@ -104,31 +110,205 @@
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
-  async function solve() {
-    const instance = await getGameInstance();
-    while(true) {
-      const map = extractMap(instance);
-      const actions = map.map((col, x) => {
-        return col.map((item, y) => {
-          const square = parseSquare(map[x][y]);
-          if (square.flag) return;
-          if (square.alreadyOpened) return;
-          if (square.bomb) return {x,y,type:'right'};
-          return {x,y,type:'left'};
-        });
-      }).flatMap(x=>x).filter(x=>x);
-      if (actions.length === 0) break;
-      const {x,y,type} = actions[Math.floor(Math.random() * actions.length)];
-		console.log('type', type);
-      if (type === 'right') {
-        rightClick(instance, x, y);
-      } else {
-        leftClick(instance, x, y);
+  function createActionsContainer(map) {
+    const actions = [];
+    const already = new Set();
+    const add = ({x, y, type}) => {
+      const hash = y * map.length + x;
+      if (already.has(hash)) return;
+      already.add(hash);
+      actions.push({x, y, type});
+    }
+    return {actions, add};
+  }
+  
+  function nextHackActions(map) {
+    const {actions, add} = createActionsContainer(map);
+    for (let x = 0; x < map.length; x++) {
+      for (let y = 0; y < map[x].length; y++) {
+        const square = map[x][y];
+        if (square.flag) continue;
+        if (square.alreadyOpened) continue;
+        if (square.bomb) {
+          add({x,y,type:'right'});
+        } else {
+        	add({x,y,type:'left'});
+        }
       }
-      await sleep(50);
+    }
+    return actions;
+  }
+  
+  function nextLegitActions(map) {
+    const {actions, add} = createActionsContainer(map);
+    for (let x = 0; x < map.length; x++) {
+      for (let y = 0; y < map[x].length; y++) {
+        const square = map[x][y];
+        if (!square.alreadyOpened) continue;
+        if (square.num === 0) continue;
+
+        const nearSquares = near(map, x, y);
+
+        if (nearSquares.length === square.num) {
+          for (const {square, x, y} of nearSquares) {
+            if (square.flag) continue;
+            add({ type: 'right', x, y });
+          }
+        } else {
+          const flaggedSquares = nearSquares.filter(({square}) => square.flag);
+          if (flaggedSquares.length >= square.num) {
+            for (const {square, x, y} of nearSquares) {
+              if (square.flag) continue;
+            	add({ type: 'left', x, y });
+            }
+          }
+        }
+      }
+    }
+    return actions;
+  }
+  
+  function near(map, x, y) {
+		let near = [];
+    if (x > 0) near.push([x-1, y]);
+    if (x > 0 && y > 0) near.push([x-1, y-1]);
+    if (x > 0 && y < map[x].length-1) near.push([x-1, y+1]);
+    if (y > 0) near.push([x, y-1]);
+    if (y < map[x].length-1) near.push([x, y+1]);
+    if (x < map.length-1) near.push([x+1, y]);
+    if (x < map.length-1 && y > 0) near.push([x+1, y-1]);
+    if (x < map.length-1 && y < map[x].length-1) near.push([x+1, y+1]);
+    return near.map(([_x, _y]) => ({x: _x, y: _y, square: map[_x][_y]})).filter(x => !x.square.alreadyOpened);
+  }
+  
+  function openSquares(map, x, y) {
+    const cur = map[x][y];
+    if (cur.bomb) return;
+    cur.alreadyOpened = true;
+    if (cur.num !== 0) return;
+    const nearSquares = near(map, x, y);
+    for (const {square, x: _x, y: _y} of nearSquares) {
+      openSquares(map, _x, _y);
     }
   }
   
-  solve();
-})()
-
+  function isSolved(map) {
+    const bombs = map.flatMap(col => col.filter(x => x.bomb)).length;
+    const flagged = map.flatMap(col => col.filter(x => !x.alreadyOpened)).length;
+    return bombs === flagged;
+	}
+  
+  function hasGenerated(map) {
+    const bombs = map.flatMap(col => col.filter(x => x.bomb)).length;
+    return bombs > 0;
+  }
+  
+  function isSolvable(map) {
+    for (let i = 0; i < 100; i++) {
+      const actions = nextLegitActions(map);
+      if (actions.length === 0) break;
+      for (const {type, x, y} of actions) {
+        if (type === 'right') {
+          map[x][y].flag = true;
+        } else if (type === 'left') {
+          openSquares(map, x, y);
+        }
+      }
+    }
+    return isSolved(map);
+	}
+  
+  async function run() {
+    document.getElementById('hack-box')?.remove();
+    const elem = document.querySelector('g-dropdown-menu');
+    const container = document.createElement('div');
+    container.id = 'hack-box';
+    container.style = 'position: absolute; top: -30px; width: 500px; display: flex';
+    elem.parentElement.insertBefore(container, elem);
+    const instance = await getGameInstance();
+    [
+      {
+        name: 'Is Lucky Game?',
+        on: () => {
+      		const rawMap = extractMap(instance);
+          const map = rawMap.map(col => col.map(parseSquare));
+          if (hasGenerated(map)) {
+          	showMessage(isSolvable(map) ? 'Solvable :)' : 'Is lucky game :(');
+          } else {
+          	showMessage('Firstly, generate the game');
+          }
+        },
+      },
+      {
+        name: 'Hack solve',
+        on: async () => {
+          let rawMap = extractMap(instance);
+          let map = rawMap.map(col => col.map(parseSquare));
+          if (!hasGenerated(map)) {
+            leftClick(instance, 0, 0);
+            await sleep(100);
+          }
+          rawMap = extractMap(instance);
+          map = rawMap.map(col => col.map(parseSquare));
+          
+          for (const {x, y, type} of nextHackActions(map)) {
+            if (type === 'right') {
+              rightClick(instance, x, y);
+            } else {
+              leftClick(instance, x, y);
+            }
+            await sleep(10);
+          }
+        },
+      },
+      {
+        name: 'Legit solve',
+        on: async () => {
+          let rawMap = extractMap(instance);
+          let map = rawMap.map(col => col.map(parseSquare));
+          if (!hasGenerated(map)) {
+            leftClick(instance, 0, 0);
+            await sleep(100);
+          }
+          for (let i = 0; i < 100; i++) {
+            const rawMap = extractMap(instance);
+            const map = rawMap.map(col => col.map(parseSquare));
+            const actions = nextLegitActions(map);
+            if (actions.length === 0) {
+              if (!isSolved(map)) {
+                showMessage('There\'s no non guessable action');
+              }
+              break;
+            }
+            for (const {x, y, type} of actions) {
+              if (type === 'right') {
+                rightClick(instance, x, y);
+              } else {
+                leftClick(instance, x, y);
+              }
+              await sleep(10);
+            }
+            await sleep(10);
+          }
+        },
+      },
+    ].forEach(({name, on}) => {
+    	const btn = document.createElement('button');
+    	btn.innerHTML = name;
+    	btn.style = 'margin-left: 10px';
+      btn.onclick = on || (() => {});
+    	container.appendChild(btn);
+    });
+    const label = document.createElement('div');
+    label.style = 'margin-left: 10px; color: red';
+    container.appendChild(label);
+    const showMessage = msg => {
+      label.innerHTML = msg;
+      setTimeout(x => {
+        if (label.innerHTML === msg) label.innerHTML = '';
+      }, 3000);
+    }
+  }
+  
+  run();
+})();
